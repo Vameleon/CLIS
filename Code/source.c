a533f18ef24a8f5a49ad8e99dee5e48866554623
Chameleon <tariq.russell.t@gmail.com>
2018-01-21 17:10:10 +0100

    added more functions for low level settings and functions for higher layers to blink sides of the leds. A deal-breaker BUG was found, trying Neopixel lib next

--- Code/source.c
+++ Code/source.c
@@ -10,21 +10,26 @@
 #define SHORT_CRGB_NUM    8         //number of leds on the strip
 
 #define LED_TYPE    WS2812          //Controller model
-#define UPDATES_PER_SECOND 100
+//#define UPDATES_PER_SECOND 500
 #define COLOR_ORDER GRB
-#define PERIOD  5000                //period in program cycle in ms
-
-
+#define PERIOD  15000                //period in program cycle in ms
+#define RED     CRGB::Red
+#define GREEN   CRGB::Green 
+#define BLACK   CRGB::Black
 CRGB SHORT_CRGB[SHORT_CRGB_NUM];    //declare our strip and give it an ID
 CRGB LONG_CRGB[LONG_CRGB_NUM];      //declare our strip and give it an ID
 
-
-#define DEF_MODE    1
-
+//---------------------End of Definition---------------------------------------//
 
 
-int LONG_BRIGHTNESS = 222;
-int SHORT_BRIGHTNESS = 222;
+
+enum MODES {NONE,BLINK_SIDES,BLINK_WHOLE,FLASH_SIDES,FLASH_WHOLE};
+MODES INDICATE_M = BLINK_SIDES;
+
+int LONG_BRIGHTNESS = 12;
+int SHORT_BRIGHTNESS = 12;
+
+
 
 //int COLORS[] = {
 // CRGB::Blue, CRGB::Orange, CRGB::Yellow, CRGB::Green, CRGB::Maroon,
@@ -34,21 +39,68 @@ int SHORT_BRIGHTNESS = 222;
 
 int COLORS[] = {
  CRGB::Blue, CRGB::Orange, CRGB::Yellow, CRGB::Green, CRGB::Maroon,
- CRGB::DarkRed,CRGB::White,CRGB::LimeGreen,CRGB::Red
+ CRGB::DarkRed,CRGB::White,CRGB::LimeGreen,CRGB::Red,CRGB::Black
 };
 int COLORS_ELE = sizeof(COLORS) / sizeof(COLORS[0]);
 
 
-
+void init();
 void pallete(void);
 void pallete2(void);
+void on_left_right(int);
+void off_left_right();
+
+
+//-------------------------End of Declaration--------------------------//
 
 
 void setup() {
  
+    void init ();
+
+}
+
+
+void loop()
+{
+
+
+
+    //pallete();
+    //pallete2();
+    Serial.printf("Program is in loop\n");
+    INDICATE(INDICATE_M);
+    Serial.printf("Current Mode is %i\n",INDICATE_M);
+
+   // on_left_right(RED);
+    //delay(300);
+    //off_left_right();
+   // FastLED.setBrightness(  LONG_BRIGHTNESS );
+    
+
+   // LONG_BRIGHTNESS=+1;
+    //if (LONG_BRIGHTNESS >=70)LONG_BRIGHTNESS=0;
+
+
+ 
+
+
+
+  
+}
+
+
+
+
+
+
+void init()
+{
     delay(1000);
     pinMode(LONG_CRGB_P, OUTPUT);
     pinMode(SHORT_CRGB_P, OUTPUT);
+    Serial.begin(115200);
+    Serial.printf("Initiating the MCU.....\n");
     delay( 3000 ); // power-up safety delay
     
     //initiate library
@@ -62,28 +114,6 @@ void setup() {
     FastLED.setBrightness(SHORT_BRIGHTNESS);
 }
 
-
-void loop()
-{
-
-
-
-    pallete();
-    pallete2();
-     FastLED.show();
-   // FastLED.setBrightness(  LONG_BRIGHTNESS );
-    
-
-   // LONG_BRIGHTNESS=+1;
-    //if (LONG_BRIGHTNESS >=70)LONG_BRIGHTNESS=0;
-
-
- 
-
-
-    delay(PERIOD);
-  
-}
 
 
 void pallete (void)
@@ -115,6 +145,108 @@ void pallete2 (void)
             SHORT_CRGB[i] = COLORS[random(COLORS_ELE)];
         }
 }
+
+
+
+
+void INDICATE (int mode)
+{
+    switch (mode)
+    {
+        case NONE:
+            off_left_right();
+            return;
+        case BLINK_SIDES:
+            
+            //start from the very last leds
+            //long
+            on_left_right(RED);
+            FastLED.delay(100);
+            off_left_right();
+            INDICATE_M = NONE;
+            return;
+
+        case BLINK_WHOLE:
+            break;
+
+            
+        case FLASH_SIDES:
+            break;
+
+        
+        case FLASH_WHOLE:
+            break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------------------------------LOW layer functions----------------------//
+
+//duration in ms
+void on_left_right(int color)
+{
+    for (int i=LONG_CRGB_NUM-8;i<LONG_CRGB_NUM;i++)
+    {
+        LONG_CRGB[i]=color;
+        yield();
+        FastLED.show();
+        yield();
+    }
+    
+   for (int i=0;i<SHORT_CRGB_NUM;i++)
+   {
+       SHORT_CRGB[i]=color;
+       yield();
+       FastLED.show();
+       yield();
+   }
+   
+
+}
+
+void off_left_right()
+{
+    for (int i=LONG_CRGB_NUM-8;i<LONG_CRGB_NUM;i++)
+    {
+        LONG_CRGB[i]=CRGB::Black;
+        yield();
+       FastLED.show();
+        yield();
+    }
+    
+    for (int i=0;i<SHORT_CRGB_NUM;i++)
+    {
+        SHORT_CRGB[i]=CRGB::Black;
+        yield();
+       FastLED.show();
+        yield();
+    }
+   
+
+}
+
+
+//-----------------------------End of LOW layer functions----------------------//
+
+
+
+
+
+
+
 
 
 // Additionl notes on FastLED compact palettes:
